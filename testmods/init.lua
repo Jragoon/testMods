0457d6c2509ac2cc7133baac32af1273d0d3a7de
Gotro <jonathangautreaux@yahoo.com>
2019-11-17 10:01:29 -0500

    Add files via upload

--- testmods/init.lua
+++ testmods/init.lua
@@ -3,3 +3,22 @@ minetest.register_node("testmods:first", {
 	tiles= {"modtests.png"},
 	groups={oddly_breakable_by_hand = 2},
 })
+
+minetest.register_craft({
+	output= "testmods:first",
+	recipe = {{"default:dirt", "default:dirt"}}
+}) 
+
+minetest.register_node("testmods:fake_diamond", {
+	description = "I cant believe this isnt diamond",
+	tiles = {"default_diamond_block.png"},
+	groups = {oddly_breakable_by_hand = 2},
+})
+
+minetest.register_craft({
+	output = "testmods:fake_diamond 99",
+	recipe = {
+		{"testmods:first"},
+		{"testmods:first"}
+	}
+})
